79cfeb7984c442bdc5241789a8af7a866212dcc2
崔棉大师 <1046166@qq.com>
2023-08-03 07:34:34 +0800

    update multicall & library

--- scripts/setting/VoltFactory/SetPauser.ts
+++ scripts/setting/VoltFactory/SetPauser.ts
@@ -9,23 +9,15 @@ const factoryAddress = "0xb33de8c0843f90655ad6249f20b473a627443d21";
 // npx hardhat run scripts/setting/VoltFactory/SetPauser.ts --network metermain
 
 async function main() {
-  const [deployer, currentAdmin] = await ethers.getSigners();
-  let receipt = await deployer.sendTransaction({
-    to: currentAdmin.address,
-    value: "10000000000000000",
-  });
-  console.log("send:", receipt.hash);
-  await receipt.wait();
+  const [deployer] = await ethers.getSigners();
 
   let factory = (await ethers.getContractAt(
     "VoltFactory",
     factoryAddress,
-    currentAdmin
+    deployer
   )) as VoltFactory;
 
-  receipt = await factory.setTreasury(newPauser);
-  console.log("setTreasury:", receipt.hash);
-  receipt = await factory.setPauser(newPauser);
+  let receipt = await factory.setPauser(newPauser);
   console.log("setPauser:", receipt.hash);
 }
 
