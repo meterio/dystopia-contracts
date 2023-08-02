b4ccf207d7f81675d25279f2ff108765965a0d60
崔棉大师 <1046166@qq.com>
2023-08-03 07:03:45 +0800

    update

--- scripts/setting/VoltFactory/SetPauser.ts
+++ scripts/setting/VoltFactory/SetPauser.ts
@@ -3,26 +3,35 @@ import { ethers } from "hardhat";
 import { Misc } from "../../Misc";
 import { VoltFactory } from "../../../typechain";
 
-const newPauser = "0x175FB9401BB5E360FF3c0Ef00097116944128930";
+const newPauser = "0x5C85A7Ae2B6d29C38cdF360553F8aCBC4e684c31";
+const factoryAddress = "0xb33de8c0843f90655ad6249f20b473a627443d21";
+
+// npx hardhat run scripts/setting/VoltFactory/SetPauser.ts --network metermain
 
 async function main() {
-  const [deployer, admin] = await ethers.getSigners();
+  const [deployer, currentAdmin] = await ethers.getSigners();
+  let receipt = await deployer.sendTransaction({
+    to: currentAdmin.address,
+    value: "10000000000000000",
+  });
+  console.log("send:", receipt.hash);
+  await receipt.wait();
 
-  const factoryJson = Misc.getContract(await deployer.getChainId(), "Factory");
-  if (factoryJson.address != ethers.constants.AddressZero) {
+  let factory = (await ethers.getContractAt(
+    "VoltFactory",
+    factoryAddress,
+    currentAdmin
+  )) as VoltFactory;
 
-    let factory = await ethers.getContractAt("VoltFactory", factoryJson.address, admin) as VoltFactory;
-
-    let receipt = await factory.setPauser( newPauser );
-    console.log("setPauser:",receipt.hash);
-  } else {
-    console.log("No factory address")
-  }
+  receipt = await factory.setTreasury(newPauser);
+  console.log("setTreasury:", receipt.hash);
+  receipt = await factory.setPauser(newPauser);
+  console.log("setPauser:", receipt.hash);
 }
 
 main()
   .then(() => process.exit(0))
-  .catch(error => {
+  .catch((error) => {
     console.error(error);
     process.exit(1);
   });
--- scripts/setting/VoltFactory/SetTreasury.ts
+++ scripts/setting/VoltFactory/SetTreasury.ts
@@ -3,7 +3,7 @@ import { ethers } from "hardhat";
 import { Misc } from "../../Misc";
 import { VoltFactory } from "../../../typechain";
 
-const newTreasury = "0xbb7ed4365d2c80e47df533e72bded9642f801ab2";
+const newTreasury = "0x5C85A7Ae2B6d29C38cdF360553F8aCBC4e684c31";
 
 async function main() {
   const [deployer, admin] = await ethers.getSigners();
