5041f4489018da0e494c39d70736071ff22240c6
崔棉大师 <1046166@qq.com>
2023-08-18 08:45:50 +0800

    update proofs

---
+++ scripts/setting/proofs.ts
@@ -0,0 +1,28 @@
+import { ethers } from "hardhat";
+import { readFileSync, writeFileSync } from "fs";
+import { MerkleTree } from "merkletreejs";
+import { BigNumber } from "ethers";
+
+const json = "./scripts/setting/leaves.json";
+const leavesIndex = 0;
+
+async function main() {
+  let jsonArr = JSON.parse(readFileSync(json).toString());
+  let leavesJson = jsonArr[leavesIndex];
+  let hashArr: any[] = [];
+  let users = leavesJson.users;
+  for (let i = 0; i < users.length; i++) {
+    console.log(users[i]);
+    writeFileSync(
+      "./scripts/setting/proofs/" + users[i].address + ".json",
+      JSON.stringify(users[i])
+    );
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
